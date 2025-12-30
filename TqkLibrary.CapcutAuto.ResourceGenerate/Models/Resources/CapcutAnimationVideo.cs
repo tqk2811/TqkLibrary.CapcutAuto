b5cfb468a28be10bab2d672d8d6b493e989ed986
tqk2811 <tqk2811@gmail.com>
2025-12-30 11:10:32 +0700

    add CapcutAnimationVideo

---
+++ TqkLibrary.CapcutAuto.ResourceGenerate/Models/Resources/CapcutAnimationVideo.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace TqkLibrary.CapcutAuto.ResourceGenerate.Models.Resources
+{
+    public sealed class CapcutAnimationVideo : CapcutAnimation
+    {
+        [JsonConstructor]
+        private CapcutAnimationVideo()
+        {
+
+        }
+        public static CapcutAnimationVideo Parse(string json_text)
+        {
+            return JsonConvert.DeserializeObject<CapcutAnimationVideo>(json_text, Singleton.JsonSerializerSettings)!;
+        }
+    }
+}
