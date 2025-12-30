b7d3673ce0ee6963c6a0cf7741a086cfe4af6158
tqk2811 <tqk2811@gmail.com>
2025-12-30 11:08:47 +0700

    add CapcutSegmentSticker

---
+++ TqkLibrary.CapcutAuto.ResourceGenerate/Models/Tracks/Segments/CapcutSegmentSticker.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using TqkLibrary.CapcutAuto.ResourceGenerate.Models.Materials;
+
+namespace TqkLibrary.CapcutAuto.ResourceGenerate.Models.Tracks.Segments
+{
+    public sealed class CapcutSegmentSticker : CapcutSegment
+    {
+        [JsonIgnore]
+        public required CapcutMaterialSticker MaterialSticker { get; init; }
+        protected override CapcutId GetMaterial()
+        {
+            return MaterialSticker;
+        }
+
+        [JsonIgnore]
+        public CapcutMaterialAnimationSticker MaterialAnimation { get; set; } = new();
+
+
+
+
+        protected override IEnumerable<CapcutId> GetExtraMaterialRefs()
+        {
+            CapcutId?[] capcutIds = new CapcutId?[]
+            {
+                MaterialAnimation,
+            };
+            foreach (CapcutId capcutId in capcutIds.Where(x => x is not null)!)
+            {
+                yield return capcutId;
+            }
+        }
+
+    }
+}
