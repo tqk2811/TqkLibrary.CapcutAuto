2762f0cbc1bb599a399b8129df452c4d8dcd0103
tqk2811 <tqk2811@gmail.com>
2025-12-30 11:11:50 +0700

    add CapcutTrackSticker

---
+++ TqkLibrary.CapcutAuto.ResourceGenerate/Models/Tracks/CapcutTrackSticker.cs
@@ -0,0 +1,14 @@
+﻿using TqkLibrary.CapcutAuto.ResourceGenerate.Enums;
+using TqkLibrary.CapcutAuto.ResourceGenerate.Models.Tracks.Segments;
+
+namespace TqkLibrary.CapcutAuto.ResourceGenerate.Models.Tracks
+{
+    public sealed class CapcutTrackSticker: CapcutTrack
+    {
+        public CapcutTrackSticker()
+        {
+            this.Type = TrackType.sticker;
+        }
+        public void Add(CapcutSegmentSticker capcutSegmentSticker) => _segments.Add(capcutSegmentSticker);
+    }
+}
