84dc6a78d86895a353c8fe114c5e1ef6c027126a
tqk2811 <tqk2811@gmail.com>
2025-12-30 11:02:01 +0700

    CapcutMaterialAnimation split to CapcutMaterialAnimationSticker & CapcutMaterialAnimationText

--- TqkLibrary.CapcutAuto.ResourceGenerate/Models/Materials/CapcutMaterialAnimation.cs
+++ TqkLibrary.CapcutAuto.ResourceGenerate/Models/Materials/CapcutMaterialAnimation.cs
@@ -5,30 +5,32 @@ using TqkLibrary.CapcutAuto.ResourceGenerate.Models.Resources;
 
 namespace TqkLibrary.CapcutAuto.ResourceGenerate.Models.Materials
 {
-    public class CapcutMaterialAnimation : CapcutMaterial
+    public abstract class CapcutMaterialAnimation<T> : CapcutMaterial
+        where T : CapcutAnimation
     {
-        readonly List<CapcutAnimation> _animations = new();
+        readonly List<T> _animations = new();
 
         [JsonProperty("animations")]
-        public IReadOnlyList<CapcutAnimation> Animations => _animations;
+        public IReadOnlyList<T> Animations => _animations;
 
         [JsonProperty("multi_language_current")]
         public string MultiLanguageCurrent { get; set; } = "none";
-
 
         public CapcutMaterialAnimation()
         {
             Type = MaterialType.sticker_animation;
         }
 
+
+
         [JsonIgnore]
-        public CapcutAnimation? In
+        public T? In
         {
             get { return _animations.FirstOrDefault(x => x.Type == AnimationType.@in); }
             set
             {
                 if (value is not null && value.Type != AnimationType.@in)
-                    throw new InvalidOperationException($"Animation type must be 'in'");
+                    throw new InvalidOperationException($"Animation type must be '{nameof(AnimationType.@in)}'");
                 var @in = In;
                 if (@in is not null)
                     _animations.Remove(@in);
@@ -38,13 +40,30 @@ namespace TqkLibrary.CapcutAuto.ResourceGenerate.Models.Materials
         }
 
         [JsonIgnore]
-        public CapcutAnimation? Out
+        public T? Out
         {
             get { return _animations.FirstOrDefault(x => x.Type == AnimationType.@out); }
             set
             {
                 if (value is not null && value.Type != AnimationType.@out)
-                    throw new InvalidOperationException($"Animation type must be 'out'");
+                    throw new InvalidOperationException($"Animation type must be '{nameof(AnimationType.@out)}'");
+                var @out = Out;
+                if (@out is not null)
+                    _animations.Remove(@out);
+                if (value is not null)
+                    _animations.Add(value);
+            }
+        }
+
+
+        [JsonIgnore]
+        public T? Loop
+        {
+            get { return _animations.FirstOrDefault(x => x.Type == AnimationType.loop); }
+            set
+            {
+                if (value is not null && value.Type != AnimationType.@out)
+                    throw new InvalidOperationException($"Animation type must be '{nameof(AnimationType.loop)}'");
                 var @out = Out;
                 if (@out is not null)
                     _animations.Remove(@out);
