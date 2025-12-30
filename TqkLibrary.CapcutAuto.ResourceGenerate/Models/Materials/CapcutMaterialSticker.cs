3dd5a92522de81ce056753e0a0280a6ec7fa19b7
tqk2811 <tqk2811@gmail.com>
2025-12-30 11:08:30 +0700

    add CapcutMaterialSticker

---
+++ TqkLibrary.CapcutAuto.ResourceGenerate/Models/Materials/CapcutMaterialSticker.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TqkLibrary.CapcutAuto.ResourceGenerate.JsonConverters;
+
+namespace TqkLibrary.CapcutAuto.ResourceGenerate.Models.Materials
+{
+    public sealed class CapcutMaterialSticker : CapcutMaterial
+    {
+        [JsonConstructor]
+        private CapcutMaterialSticker(JObject jObject) : base(jObject)
+        {
+
+        }
+
+        [JsonProperty("path")]
+        [JsonConverter(typeof(CapcutPathConverter))]
+        public required string Path { get; init; }
+
+
+        public static CapcutMaterialSticker Parse(string json)
+        {
+            return JsonConvert.DeserializeObject<CapcutMaterialSticker>(json, Singleton.JsonSerializerSettings)!;
+        }
+    }
+}
